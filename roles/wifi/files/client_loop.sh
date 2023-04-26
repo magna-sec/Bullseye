33d78ce5658bc471dfc43083b64445a8cbcd20e2
GT.Barnes <magna@magna.com>
2023-04-26 09:09:43 +0100

    Varous changes

--- roles/wifi/files/client_loop.sh
+++ roles/wifi/files/client_loop.sh
@@ -1,6 +1,6 @@
 while true; do
-    wpa_supplicant -i wlan30 -c /root/wpa_supplicant.conf &
+    wpa_supplicant -i wlan11 -c /root/wpa_supplicant_1.conf &
     sleep 45
     echo Reconnecting!
-    pkill wpa_supplicant
+    kill $(jobs -p)
 done
