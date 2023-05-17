daf31a61d24a4ab97e206a03da07155408375b5e
GT.Barnes <magna@magna.com>
2023-05-17 22:03:26 +0100

    Multiple fixes

--- roles/linux_client/files/smb_send.py
+++ roles/linux_client/files/smb_send.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 from smb.SMBConnection import SMBConnection
+import time
 
 # File
-send_file = "cat.jpg"
+send_path = "/home/smbuser/cat.jpg"
+send_name = send_path.split('/')[3]
 
 # Credentials
 userID = 'smbuser'
@@ -15,13 +17,26 @@ server_name = 'ctfd'
 domain_name = server_name
 server_ip = '192.168.64.190'
 
-# File list
-file_obj = open(send_file, 'rb')
+def send_file():
+    # File list
+    file_obj = open(send_path, 'rb')
 
-# Make connection
-conn = SMBConnection(userID, password, client_machine_name, server_name, domain=domain_name, use_ntlm_v2=True,is_direct_tcp=True)
-conn.connect(server_ip, 445)
+    # Make connection
+    conn = SMBConnection(userID, password, client_machine_name, server_name, domain=domain_name, use_ntlm_v2=True,is_direct_tcp=True)
+    conn.connect(server_ip, 445)
 
-# Send file
-conn.storeFile(service_name=sharename, path=f"\{send_file}",file_obj=file_obj)
+    # Send file
+    conn.storeFile(service_name=sharename, path=f"\{send_name}",file_obj=file_obj)
+    file_obj.close()
+    print("## File Sent ##")
+    
+
+
+def main():
+    while(True):
+        send_file()
+        time.sleep(30)
+
+if __name__ == "__main__":
+    main()
 
